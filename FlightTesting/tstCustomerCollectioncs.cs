d258075d6557c89b47f19a91f50aed53ffe8747e
mqasim212 <P2423421@my365.dmu.ac.uk>
2021-02-18 18:27:27 +0000

    created collection class for customer

---
+++ FlightTesting/tstCustomerCollectioncs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlightTesting
+{
+    [TestClass]
+    public class tstCustomerCollectioncs
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
